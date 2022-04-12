4097f86ade47e44bfb24dc4762a92e5efae1eec0
naina-is <islam.naina15@gmail.com>
2022-04-12 12:32:52 -0400

    first commit

---
+++ src/basic/Hello.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int age = 22;
+		System.out.println(age);
+	}
+	
+}
---
+++ src/basic/Hello1.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		String name = "naina";
+		System.out.println("My name is " + name);
+	}
+	
+}
